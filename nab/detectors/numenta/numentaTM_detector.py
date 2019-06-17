784a485bee764a63f6a8ac51f9016aea18784a2b
christophevvv <christophe.verdonck@gmail.com>
2019-06-17 22:03:37 +0200

    numentaTM fix

--- nab/detectors/numenta/numentaTM_detector.py
+++ nab/detectors/numenta/numentaTM_detector.py
@@ -48,30 +48,110 @@ class NumentaTMDetector(NumentaDetector):
 
 
   def initialize(self):
-    if not (self.parameters == None):
-      self.genericConfig = self.parameters["generic"]
-    # Get config params, setting the RDSE resolution
     rangePadding = abs(self.inputMax - self.inputMin) * 0.2
+    if not (self.parameters == None):
 
-    modelParams = getScalarMetricWithTimeOfDayAnomalyParams(
-      metricData=[0],
-      minVal=self.inputMin-rangePadding,
-      maxVal=self.inputMax+rangePadding,
-      minResolution=0.001,
-      tmImplementation="tm_cpp"
-    )["modelConfig"]
+      self.genericConfig = self.parameters["generic"]
+      minVal=self.inputMin-rangePadding
+      maxVal=self.inputMax+rangePadding
+      # Handle the corner case where the incoming min and max are the same
+      if minVal == maxVal:
+        maxVal = minVal + 1  
+      valueEncoder = self.parameters["modelConfig"]["modelParams"]["sensorParams"]["encoders"]["value"]
+      type = valueEncoder["type"]
+      if self.genericConfig["smartResolution"]:
+        resolution = max(0.001,(self.maxValue - self.minValue) / float(self.genericConfig["nrBuckets"]))
+      else:
+        resolution = max(0.001,(maxVal - minVal) / float(self.genericConfig["nrBuckets"]))
+      self.modelConfig = self.parameters["modelConfig"]
+      if self.genericConfig["valueOnly"]:
+        self.modelConfig["modelParams"]["sensorParams"]["encoders"]["timestamp_timeOfDay"] = None
+      valueEncoder["resolution"] = resolution
+      if type == "ScalarEncoder":
+        valueEncoder["n"] = 0
+        minVal = self.inputMin
+        maxVal = self.inputMax
+        if minVal == maxVal:
+          maxVal = minVal + 1
+        valueEncoder["minval"] = minVal
+        valueEncoder["maxval"] = maxVal
+        if "seed" in valueEncoder:
+          valueEncoder.pop("seed")
+      if type == "AdaptiveScalarEncoder":
+        if self.genericConfig["adaptiveMinMax"]:
+          valueEncoder["minval"] = minVal
+          valueEncoder["maxval"] = maxVal
+        valueEncoder.pop("resolution")
+        if "seed" in valueEncoder:
+          valueEncoder.pop("seed")
+        
+      #print valueEncoder
+      self.sensorParams = valueEncoder #To check if equal to _setupEncoderParams assignment
+    else:
+      self.modelConfig = getScalarMetricWithTimeOfDayAnomalyParams(
+        metricData=[0],
+        minVal=self.inputMin-rangePadding,
+        maxVal=self.inputMax+rangePadding,
+        minResolution=0.001,
+        tmImplementation = "cpp"
+        )["modelConfig"]
+      self._setupEncoderParams(self.modelConfig["modelParams"]["sensorParams"]["encoders"])
 
-    self._setupEncoderParams(
-      modelParams["modelParams"]["sensorParams"]["encoders"])
-
-    self.model = ModelFactory.create(modelParams)
+    #print self.sensorParams
+    self.model = ModelFactory.create(self.modelConfig)
 
     self.model.enableInference({"predictedField": "value"})
 
-    # Initialize the anomaly likelihood object
-    numentaLearningPeriod = int(math.floor(self.probationaryPeriod / 2.0))
-    self.anomalyLikelihood = anomaly_likelihood.AnomalyLikelihood(
-      learningPeriod=numentaLearningPeriod,
-      estimationSamples=self.probationaryPeriod-numentaLearningPeriod,
-      reestimationPeriod=100
-    )
+    if self.useLikelihood:
+      # Initialize the anomaly likelihood object
+      self.numentaLearningPeriod = int(math.floor(self.probationaryPeriod / 2.0))
+      self.anomalyLikelihood = anomaly_likelihood.AnomalyLikelihood(
+        learningPeriod=self.numentaLearningPeriod,
+        estimationSamples=self.probationaryPeriod-self.numentaLearningPeriod,
+        reestimationPeriod=self.genericConfig["reestimationPeriod"]
+      )
+
+
+  def _setupEncoderParams(self, encoderParams):
+    # The encoder must expect the NAB-specific datafile headers
+    encoderParams["timestamp_dayOfWeek"] = encoderParams.pop("c0_dayOfWeek")
+    encoderParams["timestamp_timeOfDay"] = encoderParams.pop("c0_timeOfDay")
+    encoderParams["timestamp_timeOfDay"]["fieldname"] = "timestamp"
+    encoderParams["timestamp_timeOfDay"]["name"] = "timestamp"
+    encoderParams["timestamp_weekend"] = encoderParams.pop("c0_weekend")
+    encoderParams["value"] = encoderParams.pop("c1")
+    encoderParams["value"]["fieldname"] = "value"
+    encoderParams["value"]["name"] = "value"
+ 
+    self.sensorParams = encoderParams["value"]
+    
+    
+    
+#     
+#     if not (self.parameters == None):
+#       self.genericConfig = self.parameters["generic"]
+#     # Get config params, setting the RDSE resolution
+#     rangePadding = abs(self.inputMax - self.inputMin) * 0.2
+# 
+#     modelParams = getScalarMetricWithTimeOfDayAnomalyParams(
+#       metricData=[0],
+#       minVal=self.inputMin-rangePadding,
+#       maxVal=self.inputMax+rangePadding,
+#       minResolution=0.001,
+#       tmImplementation="tm_cpp"
+#     )["modelConfig"]
+# 
+#     self._setupEncoderParams(
+#       modelParams["modelParams"]["sensorParams"]["encoders"])
+# 
+#     self.model = ModelFactory.create(modelParams)
+# 
+#     self.model.enableInference({"predictedField": "value"})
+# 
+#     # Initialize the anomaly likelihood object
+#     numentaLearningPeriod = int(math.floor(self.probationaryPeriod / 2.0))
+#     self.anomalyLikelihood = anomaly_likelihood.AnomalyLikelihood(
+#       learningPeriod=numentaLearningPeriod,
+#       estimationSamples=self.probationaryPeriod-numentaLearningPeriod,
+#       reestimationPeriod=100
+#     )
